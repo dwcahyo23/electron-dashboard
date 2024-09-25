36f54f005f69a871888ec54fe83399de2dfebc91
Yusuf Dwi <dwcahyo23@gmail.com>
2024-09-25 09:09:23 +0700

    change title

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,5 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
-// import fixReactVirtualized from 'esbuild-plugin-react-virtualized'
 import { resolve } from 'path'
 
 export default defineConfig({
@@ -11,11 +10,6 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
-    // optimizeDeps: {
-    //   esbuildOptions: {
-    //     plugins: [fixReactVirtualized]
-    //   }
-    // },
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src')
